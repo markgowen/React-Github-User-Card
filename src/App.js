50f6adc736972b880c19a0f6dfd167927897d903
Mark Gowen <mark.gowen78@gmail.com>
2019-10-01 13:25:12 -0700

    updated App.js
    added Material UI style components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+// Material UI
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+
+const useStyles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    backgroundColor: '#24292d',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+
+
+
+
+
+
+class App extends React.Component {
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            GitHub Followers
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(useStyles) (App);

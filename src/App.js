c9fa9f551dd920e551d14b75e84f1423328ffa20
Mark Gowen <mark.gowen78@gmail.com>
2019-10-01 16:48:14 -0700

    completed project
    completed project with styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from 'react'
 import axios from 'axios'
 
 // Material UI
+import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -10,9 +11,13 @@ import MenuIcon from '@material-ui/icons/Menu'
 import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core/styles'
 
+// Components
+import GitHubCard from './components/GitHubCard'
+
 const useStyles = theme => ({
   root: {
     flexGrow: 1,
+    margin: '75px auto'
   },
   toolbar: {
     backgroundColor: '#24292d',
@@ -23,17 +28,65 @@ const useStyles = theme => ({
   title: {
     flexGrow: 1,
   },
+  gridItem: {
+    padding: theme.spacing(2)
+  },
+  container: {
+    marginTop: -80
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
 });
 
 
-
-
-
-
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      followers: [],
+    };
+    console.log('Constructor is running!')
+  }
+
+  componentDidMount() {
+    console.log('cDM is running and fetching GitHub Users');
+
+    axios
+      .get('https://api.github.com/users/markgowen')
+      .then(res => {
+        this.setState({
+          user: [res.data]
+        });
+      })
+      .catch(err => console.log(err));
+
+      axios
+      .get('https://api.github.com/users/markgowen/followers')
+      .then(res => {
+        const tempFollowers = res.data;
+        tempFollowers.forEach(data => {
+          axios
+          .get(data.url)
+          .then(res => {
+            this.setState({
+              followers: [...this.state.followers, res.data]
+              })
+          })
+        });
+      })
+      .catch(err => console.log(err));
+ }
+
+  componentDidUpdate() {
+    console.log('cDU is updating...')
+  }
 
   render() {
+    console.log('rendering...');
     const { classes } = this.props;
     return (
       <div className={classes.root}>
@@ -43,11 +96,43 @@ class App extends React.Component {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            GitHub Followers
+            GitHub Users
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Grid container className={classes.root} justify="center">
+        {this.state.user.map(data => {
+          return (
+            <Grid item className={classes.gridItem} s>
+              <GitHubCard
+                key={data.id}
+                avatar={data.avatar_url}
+                name={data.name}
+                bio={data.bio}
+                location={data.location}
+                followersCount={data.followers}
+                followingCount={data.following}
+              />
+            </Grid>
+          );
+        })}
+        {this.state.followers.map(data => {
+          return (
+            <Grid item className={classes.gridItem} s>
+              <GitHubCard
+                key={data.id}
+                avatar={data.avatar_url}
+                name={data.name}
+                bio={data.bio}
+                location={data.location}
+                followersCount={data.followers}
+                followingCount={data.following}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
       </div>
     );
   }
